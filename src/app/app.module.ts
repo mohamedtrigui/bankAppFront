e63bd1cca90183b4fa7f66c8e21f4cc6cf1d6edb
Mohamed Trigui <mohamedtrigui718@gmail.com>
2021-09-26 22:48:27 +0100

    Add the sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [	AppComponent,
+      SidebarComponent
+   ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, NgbTooltipModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
